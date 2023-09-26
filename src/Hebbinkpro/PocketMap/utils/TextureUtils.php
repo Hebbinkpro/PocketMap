8e2c6d81d6678d1ac406ef1c88273e8eb26f0f19
Hebbinkpro <jorim.hebbink@gmail.com>
2023-09-26 12:19:51 +0200

    Update License

--- src/Hebbinkpro/PocketMap/utils/TextureUtils.php
+++ src/Hebbinkpro/PocketMap/utils/TextureUtils.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ *   _____           _        _   __  __
+ *  |  __ \         | |      | | |  \/  |
+ *  | |__) |__   ___| | _____| |_| \  / | __ _ _ __
+ *  |  ___/ _ \ / __| |/ / _ \ __| |\/| |/ _` | '_ \
+ *  | |  | (_) | (__|   <  __/ |_| |  | | (_| | |_) |
+ *  |_|   \___/ \___|_|\_\___|\__|_|  |_|\__,_| .__/
+ *                                            | |
+ *                                            |_|
+ *
+ * Copyright (C) 2023 Hebbinkpro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 namespace Hebbinkpro\PocketMap\utils;
 
