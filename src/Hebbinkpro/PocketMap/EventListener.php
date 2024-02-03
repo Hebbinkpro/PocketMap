21e7752bb8b6df4b9e652a7a3d37ff8bae4e6152
Hebbinkpro <jorim.hebbink@gmail.com>
2024-02-03 12:24:06 +0100

    Update copyright

--- src/Hebbinkpro/PocketMap/EventListener.php
+++ src/Hebbinkpro/PocketMap/EventListener.php
@@ -10,7 +10,7 @@
  *                                            | |
  *                                            |_|
  *
- * Copyright (C) 2023 Hebbinkpro
+ * Copyright (c) 2024 Hebbinkpro
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
